2701728f61815604775f3d9e122e5e77d1e7e894
Josh Levinger <josh@levinger.net>
2020-09-21 10:37:45 -0700

    check .env

--- follower_count.py
+++ follower_count.py
@@ -4,14 +4,20 @@ import csv, codecs
 import argparse
 import tweepy
 
-# access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
-# access_token_secret = os.environ.get('TWITTER_ACCESS_SECRET')
 consumer_key = os.environ.get('TWITTER_CONSUMER_TOKEN')
 consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+if consumer_key and consumer_secret:
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+else:
+    print('need to set TWITTER_CONSUMER_TOKEN and TWITTER_CONSUMER_SECRET in .env')
+    sys.exit(-1)
+
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_SECRET')
+if access_token and access_token_secret:
+    auth.set_access_token(access_token, access_token_secret)
 
 def open_csv(filename):
     targets = []
